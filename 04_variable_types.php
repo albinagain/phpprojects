313f176795fd6bbbc65a88d665116bacd4897e73
LGR <wis2023@S312.uc-bcf.edu.ph>
2023-09-27 17:56:09 +0800

    variable types

---
+++ 04_variable_types.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable Types</title>
+</head>
+<body>
+    <?php 
+        $int_var = 123;
+        $double_var = 123.456;
+        $null_var = NULL;
+        if($int_var){
+            print "The boolean is true";
+            print "<br>";
+        }
+        else{
+            print "The boolean is false";
+            print "<br>";
+        }
+    ?>
+</body>
+</html>
