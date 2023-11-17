0f88cfc0423fe01357af73d3a24bc50fa7075dcc
albin <ajmmosawan@gmail.com>
2023-11-17 17:23:31 +0800

    completed table with bootstrap

--- test_sql.php
+++ test_sql.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SQL Connect</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 <body>
     <?php
@@ -18,59 +19,131 @@
             die("Connection failed: " . $conn->connect_error);
         }
     ?>
-    <table>
-        <tr>
-            <td>Student ID</td>
-            <td>First Name</td>
-            <td>Last Name</td>
-            <td>Date of Birth</td>
-            <td>Email</td>
-            <td>Phone</td>
-        </tr>
-        <?php
-            $sqlStudent = "SELECT * FROM Student";
-            $resultStudent = $conn->query($sqlStudent);
 
-            if ($resultStudent){
-                echo "Table: Student<br><br>";
-                while ($row = $resultStudent->fetch_assoc()) {
-                    echo "<tr><td>" .$row["StudentID"]. "</td>";
-                    echo "<td>" .$row["FirstName"]. "</td>";
-                    echo "<td>" .$row["LastName"]. "</td>";
-                    echo "<td>" .$row["DateOfBirth"]. "</td>";
-                    echo "<td>" .$row["Email"]. "</td>";
-                    echo "<td>" .$row["Phone"]. "</td></tr>";
+    <div class="container">
+        <h2>Table: Student</h2>
+        <table class="table table-striped">
+            <tr>
+                <td>Student ID</td>
+                <td>First Name</td>
+                <td>Last Name</td>
+                <td>Date of Birth</td>
+                <td>Email</td>
+                <td>Phone</td>
+            </tr>
+            <?php
+                $sqlStudent = "SELECT * FROM Student";
+                $resultStudent = $conn->query($sqlStudent);
+
+                if ($resultStudent){
+                    while ($row = $resultStudent->fetch_assoc()) {
+                        echo "<tr><td>" .$row["StudentID"]. "</td>";
+                        echo "<td>" .$row["FirstName"]. "</td>";
+                        echo "<td>" .$row["LastName"]. "</td>";
+                        echo "<td>" .$row["DateOfBirth"]. "</td>";
+                        echo "<td>" .$row["Email"]. "</td>";
+                        echo "<td>" .$row["Phone"]. "</td></tr>";
+                    }
                 }
-            }
-            else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-        ?>
-    </table>
-
-    <table>
-        <tr>
-            <td>Course ID</td>
-            <td>Course Name</td>
-            <td>Credits</td>
-        </tr>
-        <?php
-        echo "<br>";
-            $sqlCourse = "SELECT * FROM Course";
-            $resultCourse = $conn->query($sqlCourse);
-
-            if ($resultCourse){
-                echo "Table: Course<br><br>";
-                while ($row = $resultCourse->fetch_assoc()) {
-                    echo "<tr><td>" .$row["CourseID"]. "</td>";
-                    echo "<td>" .$row["CourseName"]. "</td>";
-                    echo "<td>" .$row["Credits"]. "</td></tr>";
+                else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
                 }
-            }
-            else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-        ?>
-    </table>
+            ?>
+        </table>
+    </div>
+
+    <div class="container">
+        <h2>Table: Course</h2>
+        <table class="table table-striped">
+            <tr>
+                <td>Course ID</td>
+                <td>Course Name</td>
+                <td>Credits</td>
+            </tr>
+            <?php
+                echo "<br>";
+                $sqlCourse = "SELECT * FROM Course";
+                $resultCourse = $conn->query($sqlCourse);
+
+                if ($resultCourse){
+                    while ($row = $resultCourse->fetch_assoc()) {
+                        echo "<tr><td>" .$row["CourseID"]. "</td>";
+                        echo "<td>" .$row["CourseName"]. "</td>";
+                        echo "<td>" .$row["Credits"]. "</td></tr>";
+                    }
+                }
+                else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            ?>
+        </table>
+    </div>
+
+    <div class="container">
+        <h2>Table: Instructor</h2>
+        <table class="table table-striped">
+            <tr>
+                <td>Instructor ID</td>
+                <td>First Name</td>
+                <td>Last Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+
+            </tr>
+            <?php
+                echo "<br>";
+                $sqlInst = "SELECT * FROM Instructor";
+                $resultInst = $conn->query($sqlInst);
+
+                if ($resultInst){
+                    while ($row = $resultInst->fetch_assoc()) {
+                        echo "<tr><td>" .$row["InstructorID"]. "</td>";
+                        echo "<td>" .$row["FirstName"]. "</td>";
+                        echo "<td>" .$row["LastName"]. "</td>";
+                        echo "<td>" .$row["Email"]. "</td>";
+                        echo "<td>" .$row["Phone"]. "</td></tr>";
+                    }
+                }
+                else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            ?>
+        </table>
+    </div>
+    
+    <div class="container">
+        <h2>Table: Enrollment</h2>
+        <table class="table table-striped">
+            <tr>
+                <td>Enrollment ID</td>
+                <td>Student ID</td>
+                <td>Course ID</td>
+                <td>Enrollemnt Date</td>
+                <td>Grade</td>
+
+            </tr>
+            <?php
+                echo "<br>";
+                $sqlEnroll = "SELECT * FROM Enrollment";
+                $resultEnroll = $conn->query($sqlEnroll);
+
+                if ($resultEnroll){
+                    while ($row = $resultEnroll->fetch_assoc()) {
+                        echo "<tr><td>" .$row["EnrollmentID"]. "</td>";
+                        echo "<td>" .$row["StudentID"]. "</td>";
+                        echo "<td>" .$row["CourseID"]. "</td>";
+                        echo "<td>" .$row["EnrollmentDate"]. "</td>";
+                        echo "<td>" .$row["Grade"]. "</td></tr>";
+                    }
+                }
+                else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+                $conn->close();
+            ?>
+        </table>
+    </div>
+    
+
 </body>
 </html>
